ca29347d747ab8f687164528c6e038c60e835b7a
sunill27 <sunilchand675@gmail.com>
2024-10-16 10:08:06 +0545

    added updatedToDo

--- src/todo/todoController.ts
+++ src/todo/todoController.ts
@@ -1,18 +1,29 @@
 import { Socket } from "socket.io";
 import { getSocketIo } from "../../server";
 import todoModel from "./todoModel";
+import { ITodo, Status } from "./todoTypes";
 
 class Todo {
   private io = getSocketIo();
   constructor() {
     this.io.on("connection", (socket: Socket) => {
       console.log("New client connected!!");
+
+      //To receive data while performing add operation:
       socket.on("addToDo", (data) => this.handleAddToDo(socket, data));
+
+      //To receive data while performing delete operation:
+      socket.on("deleteToDo", (data) => this.handleDeleteToDo(socket, data));
+
+      //To receive data while performing delete operation:
+      socket.on("updateToDoStatus", (data) =>
+        this.handleUpdateToDo(socket, data)
+      );
     });
   }
 
   //Add To Do:
-  private async handleAddToDo(socket: Socket, data: any) {
+  private async handleAddToDo(socket: Socket, data: ITodo) {
     try {
       const { task, deadline, status } = data;
       await todoModel.create({
@@ -21,6 +32,31 @@ class Todo {
         status,
       });
 
+      const todo = await todoModel.find({ status: Status.PENDING });
+      socket.emit("todo_updated", {
+        status: "success",
+        data: todo,
+      });
+    } catch (error) {
+      socket.emit("todo_response", {
+        status: "error",
+        error,
+      });
+    }
+  }
+
+  //Delete To Do:
+  private async handleDeleteToDo(socket: Socket, data: { id: string }) {
+    try {
+      const { id } = data;
+      const deletedToDo = await todoModel.findByIdAndDelete(id);
+      if (!deletedToDo) {
+        socket.emit("todo_response", {
+          status: "error",
+          message: "ToDo not found!",
+        });
+        return;
+      }
       const todo = await todoModel.find();
       socket.emit("todo_updated", {
         status: "success",
@@ -33,6 +69,37 @@ class Todo {
       });
     }
   }
-}
 
+  //Update To Do:
+  private async handleUpdateToDo(
+    socket: Socket,
+    data: { id: string; status: Status }
+  ) {
+    try {
+      const { id, status } = data;
+      const updatedToDo = await todoModel.findByIdAndUpdate(
+        id,
+        { status },
+        { new: true }
+      );
+      if (!updatedToDo) {
+        socket.emit("todo_response", {
+          status: "error",
+          message: "ToDo not found!",
+        });
+        return;
+      }
+      const todo = await todoModel.find({ status: Status.PENDING });
+      socket.emit("todo_updated", {
+        status: "success",
+        data: todo,
+      });
+    } catch (error) {
+      socket.emit("todo_response", {
+        status: "error",
+        error,
+      });
+    }
+  }
+}
 export default new Todo();
